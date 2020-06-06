ee93d3f16f1a5d1b201fe6c1157dcfc693576390
Vishal-Malo <vishalmalo272000@gmail.com>
2020-06-06 12:11:49 +0530

    Linear Regression Class using Gradient Descent

--- LinearRegressionClassGD.py
+++ LinearRegressionClassGD.py
@@ -3,32 +3,28 @@ class LinearRegression:
         self.m = 1
         self.b = 0
 
-    def ss(self, m, b):
-        sum = 0
-        if m == 1:
-            for i in range(len(self.x_train)):
-                sum = sum + 2*self.x_train[i] * (self.m * self.x_train[i] + self.b - self.y_train[i])
-        elif b == 1:
-            for i in range(len(self.x_train)):
-                sum = sum + 2*(self.m * self.x_train[i] + self.b - self.y_train[i])
+    def calculate_stepsize(self, m, b):
+        s = 0
+        if m:
+            s = 2 * sum(self.x_train * (self.m * self.x_train + self.b - self.y_train))
+        elif b:
+            s = 2 * sum((self.m * self.x_train + self.b - self.y_train))
 
-        return sum
+        return s
 
     def fit(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
 
-        lr = 0.01
+        learning_rate = 0.0001
         count = 0
         while count <= 1000:
-            ssm = self.ss(m=1, b=0)
-            ssb = self.ss(m=0, b=1)
-            step_m = ssm * lr
-            step_b = ssb * lr
-            m = self.m - step_m
-            b = self.b - step_b
-            self.m = m
-            self.b = b
+            ssm = self.calculate_stepsize(m=True, b=False)
+            ssb = self.calculate_stepsize(m=False, b=True)
+            step_m = ssm * learning_rate
+            step_b = ssb * learning_rate
+            self.m = self.m - step_m
+            self.b = self.b - step_b
             count = count + 1
             if -0.01 < step_m < 0.01 or -0.01 < step_b < 0.01:
                 break
